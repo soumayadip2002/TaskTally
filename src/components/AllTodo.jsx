8cd5d3c370e5f26fe843f59bd604f191a431aa5f
soumayadip2002 <soumayadipsaha2002@gmail.com>
2023-11-20 08:16:59 +0530

    update

--- src/components/AllTodo.jsx
+++ src/components/AllTodo.jsx
@@ -5,6 +5,7 @@ import { IoIosRadioButtonOff } from "react-icons/io";
 import { useTask } from "../lib/context/Tasks";
 import { useUser } from "../lib/context/UserContext";
 import EditTodo from "./EditTodo";
+import { AnimatePresence, motion } from "framer-motion";
 const AllTodo = () => {
   const { tasks, completeTask, deleteTask, deleteCompletedTask } = useTask();
   const { user } = useUser();
@@ -12,10 +13,12 @@ const AllTodo = () => {
 
   const [completedTask, setCompletedTask] = useState([]);
 
-  useEffect(()=>{
-    const completed = tasks.filter((task)=>task.userId===user.$id && task.completed);
-    setCompletedTask(completed)
-  }, [tasks, user.$id])
+  useEffect(() => {
+    const completed = tasks.filter(
+      (task) => task.userId === user.$id && task.completed
+    );
+    setCompletedTask(completed);
+  }, [tasks, user.$id]);
 
   const filteredTask = tasks.filter((task) => {
     if (filter === "All") return true;
@@ -37,12 +40,12 @@ const AllTodo = () => {
       <div className="dark:border-gray-500 border-gray-300  border-b-[1px]">
         <div className="flex justify-between sm:justify-center items-center p-4 text-sm text-gray-400 cursor-pointer">
           <p className="sm:hidden">{incompleteTask.length} items left</p>
-          <div className="flex gap-x-5">
+          <div className="flex gap-x-5   ease-in delay-100 transition-all">
             <p
               onClick={() => {
                 setFilter("All");
               }}
-              className={filter === "All" ? "text-cyan-600" : ""}
+              className={` ${filter === "All" ? "text-cyan-600" : ""}`}
             >
               All
             </p>
@@ -66,7 +69,7 @@ const AllTodo = () => {
           <p
             className="sm:hidden cursor-pointer"
             onClick={() => {
-              deleteCompletedTask(completedTask)
+              deleteCompletedTask(completedTask);
               setCompletedTask(null);
             }}
           >
@@ -75,55 +78,62 @@ const AllTodo = () => {
         </div>
       </div>
 
-      <ul>
-        {filteredTask.map((task) =>
-          task.userId === user.$id ? (
-            <li
-              key={task.$id}
-              className="dark:border-gray-500 border-gray-300 border-b-[1px] last:border-none"
-            >
-              <div className="flex justify-between items-center p-3 text-sm gap-x-5">
-                <div className="flex items-center text-sm gap-x-5">
-                  <div
-                    className={`flex items-center justify-center cursor-pointer `}
-                    onClick={() => handleUpdate(task)}
-                  >
-                    {task.completed ? (
-                      <div className="border-[1px] p-[.1rem] grid place-items-center rounded-full bg-gradient-to-br from-[#57ddff] to-[#c058f3] border-none">
-                        <AiOutlineCheck className="text-2xl p-[.3rem] text-white font-bold" />
+      <ul className="overflow-hidden">
+        <AnimatePresence>
+          {filteredTask.map((task) =>
+            task.userId === user.$id ? (
+              <motion.div
+                key={task.$id}
+                layout
+              >
+                <li
+                  key={task.$id}
+                  className="dark:border-gray-500 border-gray-300 border-b-[1px] last:border-none"
+                >
+                  <div className="flex justify-between items-center p-3 text-sm gap-x-5">
+                    <div className="flex items-center text-sm gap-x-5">
+                      <div
+                        className={`flex items-center justify-center cursor-pointer `}
+                        onClick={() => handleUpdate(task)}
+                      >
+                        {task.completed ? (
+                          <div className="border-[1px] p-[.1rem] grid place-items-center rounded-full bg-gradient-to-br from-[#57ddff] to-[#c058f3] border-none">
+                            <AiOutlineCheck className="text-2xl p-[.3rem] text-white font-bold" />
+                          </div>
+                        ) : (
+                          <div className="border-[1px] rounded-full grid place-items-center border-gray-400 overflow-hidden">
+                            <IoIosRadioButtonOff className="text-2xl text-white dark:text-[#2c363f]" />
+                          </div>
+                        )}
                       </div>
-                    ) : (
-                      <div className="border-[1px] rounded-full grid place-items-center border-gray-400 overflow-hidden">
-                        <IoIosRadioButtonOff className="text-2xl text-white dark:text-[#2c363f]" />
+
+                      <div
+                        className={`text-lg flex items-center pt-1  ${
+                          task.completed ? "line-through text-gray-400" : ""
+                        }`}
+                      >
+                        <p className="overflow-y-auto overflow-x-hidden">
+                          {task.task}
+                        </p>
                       </div>
-                    )}
+                    </div>
+
+                    <div className="flex gap-x-5 text-xl sm:text-base cursor-pointer">
+                      {!task.completed ? <EditTodo task={task} /> : ""}
+
+                      <RxCross1
+                        className="text-red-500"
+                        onClick={() => deleteTask(task.$id)}
+                      />
+                    </div>
                   </div>
-
-                  <div
-                    className={`text-lg flex items-center pt-1  ${
-                      task.completed ? "line-through text-gray-400" : ""
-                    }`}
-                  >
-                    <p className="overflow-y-auto overflow-x-hidden">
-                      {task.task}
-                    </p>
-                  </div>
-                </div>
-
-                <div className="flex gap-x-5 text-xl sm:text-base cursor-pointer">
-                  {!task.completed ? <EditTodo task={task} /> : ""}
-
-                  <RxCross1
-                    className="text-red-500"
-                    onClick={() => deleteTask(task.$id)}
-                  />
-                </div>
-              </div>
-            </li>
-          ) : (
-            ""
-          )
-        )}
+                </li>
+              </motion.div>
+            ) : (
+              ""
+            )
+          )}
+        </AnimatePresence>
       </ul>
     </div>
   );
